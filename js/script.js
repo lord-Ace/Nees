a93da96c51edd2e3cac439e11e3c671453428a0b
lord-Ace <109697240+lord-Ace@users.noreply.github.com>
2024-09-26 14:26:47 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var currrenttime = new Date();
+  const themes = document.getElementById("state");
+  window.onload = function () {    
+      const currentTheme = localStorage.getItem("savedTheme");
+      if (currentTheme === "true") {
+          themes.checked = true;
+          document
+              .querySelectorAll("body")
+              .forEach(sections => sections.classList.add("dark"));
+          document
+              .querySelectorAll(".content")
+              .forEach(content => content.classList.add("drk"));
+      } else if (currentTheme === "false") {
+          themes.checked = false;
+          document
+              .querySelectorAll("body")
+              .forEach(sections => sections.classList.remove("dark"));
+          document
+              .querySelectorAll(".content")
+              .forEach(content => content.classList.remove("drk"));
+      }
+      document.querySelectorAll(".active-btn").forEach(btn => document.getElementById("dropdown-label").innerHTML += btn.textContent)
+  };
+  themes.addEventListener("input", function () {
+    if (this.checked) {
+        document
+            .querySelectorAll("body")
+            .forEach(sections => sections.classList.add("dark"));
+        document
+            .querySelectorAll(".content")
+            .forEach(content => content.classList.add("drk"));
+    } else {
+        document
+            .querySelectorAll("body")
+            .forEach(sections => sections.classList.remove("dark"));
+        document
+            .querySelectorAll(".content")
+            .forEach(content => content.classList.remove("drk"));
+    }
+    localStorage.setItem("savedTheme", themes.checked ? "true" : "false");
+});
+  var currrenttime = new Date();
     var hours = currrenttime.getHours().toString().padStart(2, "0");
     var minutes = currrenttime.getMinutes().toString().padStart(2, "0");
     var timeFormat = hours + ":" + minutes;
@@ -9,28 +49,6 @@ document.addEventListener("DOMContentLoaded", function () {
     currentYear.textContent = new Date().getFullYear();
     const buttons = document.querySelectorAll(".nav");
     const tabs = document.querySelectorAll(".main");
-    const themes = document.getElementById("state");
-    window.onload = function () {
-      document.querySelectorAll(".active-btn").forEach(btn => document.getElementById("dropdown-label").innerHTML += btn.textContent)
-        const currentTheme = localStorage.getItem("savedTheme");
-        if (currentTheme === "true") {
-            themes.checked = true;
-            document
-                .querySelectorAll("body")
-                .forEach(sections => sections.classList.add("dark"));
-            document
-                .querySelectorAll(".content")
-                .forEach(content => content.classList.add("drk"));
-        } else if (currentTheme === "false") {
-            themes.checked = false;
-            document
-                .querySelectorAll("body")
-                .forEach(sections => sections.classList.remove("dark"));
-            document
-                .querySelectorAll(".content")
-                .forEach(content => content.classList.remove("drk"));
-        }
-    };
     let currentImage = 0
         const images = document.querySelectorAll(".img-box")
         const prev = document.getElementById("prev")
@@ -69,24 +87,6 @@ document.addEventListener("DOMContentLoaded", function () {
           clearInterval(autoSlide)
           autoSlide = setInterval(nextSlide, timer)
         }
-    themes.addEventListener("input", function () {
-        if (this.checked) {
-            document
-                .querySelectorAll("body")
-                .forEach(sections => sections.classList.add("dark"));
-            document
-                .querySelectorAll(".content")
-                .forEach(content => content.classList.add("drk"));
-        } else {
-            document
-                .querySelectorAll("body")
-                .forEach(sections => sections.classList.remove("dark"));
-            document
-                .querySelectorAll(".content")
-                .forEach(content => content.classList.remove("drk"));
-        }
-        localStorage.setItem("savedTheme", themes.checked ? "true" : "false");
-    });
     function activateTab(index) {
         buttons.forEach(btn => btn.classList.remove("active-btn"));
         tabs.forEach(tb => tb.classList.remove("active-tab"));
